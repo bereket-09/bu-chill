ed67e3ea56e67ef2e3578d9631a3744256f1d9b0
Bereket.Zelalem <bereketyezelalem@gmail.com>
2026-09-21 23:42:53 +0300

    feat(home): remove Continue Your Journey section from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { NextPage } from "next";
 import dynamic from "next/dynamic";
 
 const HomeHero = dynamic(() => import("@/components/sections/Home/Hero/Hero"));
-const ContinueWatching = dynamic(() => import("@/components/sections/Home/ContinueWatching"));
 const HomePageList = dynamic(() => import("@/components/sections/Home/List"));
 
 const HomePage: NextPage = () => {
@@ -13,7 +12,6 @@ const HomePage: NextPage = () => {
 
       {/* Netflix-Style Overlapping Content Rows */}
       <div className="relative z-20 flex flex-col gap-6 md:gap-10 mt-4 md:-mt-20 px-4 sm:px-8 md:pl-24 md:pr-10 pb-32 md:pb-20">
-        <ContinueWatching />
         <HomePageList />
       </div>
     </div>
